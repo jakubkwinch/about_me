af9a9b7d7a2b8539eb4730c37095f07ea5e79862
jakubkwinch <36926437+jakubkwinch@users.noreply.github.com>
2019-02-15 08:55:02 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+print("""
+Hello, World!
+My name is Jakub Winch
+and I am from Norwalk, CT. 
+""")
+
+print("""
+Here's a picture of a dog:
+o______o
+ ||||||
+""")
+
+born = input('What year were you born?')
+born = int(born)
+age = (2025-born)
+print("In 2025 you'll be:", age, "years old!")
